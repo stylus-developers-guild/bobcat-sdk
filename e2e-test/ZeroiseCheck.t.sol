f007bdf1c5a43a9c4bc2fd7424492899b0902ea7
bayge <alex@fluidity.money>
2026-08-04 17:42:06 +0930

    Fix unused variable

--- e2e-test/ZeroiseCheck.t.sol
+++ e2e-test/ZeroiseCheck.t.sol
@@ -10,7 +10,7 @@ contract ZeroiseCheck is Test {
         address z = IArbFoundry(address(vm)).deployStylusCode(
             "e2e-test/zeroise-check.wasm"
         );
-        (bool rc, bytes memory rd) = z.call("");
+        (,bytes memory rd) = z.call("");
         for (uint i = 0; i < 1024; ++i) {
             assertEq(0, rd[i]);
         }
